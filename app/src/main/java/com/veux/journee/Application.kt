96fb08b263e59dabfd71be49b9c2a2ed7f6043a6
bekachachua <beka.chachua@space.ge>
2023-02-15 22:07:34 +0400

    # Create Application.kt for implementing startup classes

---
+++ app/src/main/java/com/veux/journee/Application.kt
@@ -0,0 +1,10 @@
+package com.veux.journee
+
+import android.app.Application
+
+class App: Application() {
+    override fun onCreate() {
+        super.onCreate()
+        // Initialize necessary components here
+    }
+}
